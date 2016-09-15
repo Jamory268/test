e996f455bf9c80b495e09e8a85869e16a38e346b
Jeron Amory <Jeron Amory>
2016-09-14 20:21:29 -0500

    Practice 'for' loop coding

---
+++ Practice.py
@@ -0,0 +1,9 @@
+print "Even numbers up to 100 are:"
+for x in range (0, 102, 2):
+    print(x) 
+
+List = ['And', 'now', 'for', 'odd', 'numbers:']
+print(List)
+for y in range (0, 100, 2):
+    y +=1
+    print(y)
