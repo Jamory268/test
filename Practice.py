d04bc842bb536d934512224ad5218cf784878bdb
Jeron Amory <j.amory268@gmail.com>
2016-09-14 20:28:55 -0500

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -3,7 +3,9 @@ for x in range (0, 102, 2):
     print(x) 
 
 List = ['And', 'now', 'for', 'odd', 'numbers:']
-print(List)
+for i in range (0, len(List)):
+    print(List[i])
+
 for y in range (0, 100, 2):
     y +=1
     print(y)
